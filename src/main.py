1c1598df7be525f7307759076fc7ceb647d63aaa
Joseph-Essa <josepheissa0@gmail.com>
2025-07-02 19:18:07 +0300

    create main

---
+++ src/main.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from beautifulsoup_scripts.job_scraping import scrape_jobs
+from beautifulsoup_scripts.UI import show_header, get_user_input, show_results, show_error
+
+def main():
+    show_header()
+
+    search_query, max_jobs = get_user_input()
+
+    if st.button("Start Scraping"):
+        base_url = f"https://wuzzuf.net/search/jobs/?a=navbl%7Cspbl&q={search_query.replace(' ', '%20')}&start"
+        try:
+            with st.spinner("Scraping jobs..."):
+                df = scrape_jobs(base_url=base_url, max_jobs=max_jobs)
+                if df.empty:
+                    show_error("No jobs found or scraping failed.")
+                else:
+                    show_results(df)
+        except Exception as e:
+            show_error(str(e))
+
+if __name__ == "__main__":
+    main()
